7d117205346af51ac598d10afe8dfb5c97d9b0c5
John Brown <100054328@csisd.org>
2023-10-03 10:55:30 -0500

    Complete rework of core systems

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Game {
 
-  static void options(String optionList) {
+  static void Options(String optionList) {
     //Defining Things
     String optionRemain = optionList;
     String optionPart = "An issue occured";
@@ -24,6 +24,46 @@ public class Game {
     System.out.print("Choice: ");
   }
 
+  static int Response() {
+    Scanner response = new Scanner(System.in);
+    return Integer.parseInt(response.nextLine());
+  }
+
+  static void CallOption(String loadOption) {
+    switch (loadOption) {
+      case "startPlatform":
+        StartPlatform();
+        break;
+      case "leftPath":
+        LeftPath();
+        break;
+      case "rightPath":
+        //load method
+        break;
+      default:
+        NotAnOption();
+    }
+
+  }
+
+  static void StartPlatform() {
+    System.out.println("You find yourself standing on a mysterious platform in a clearing. Behind you is a cliff. Ahead of you there are two paths.");
+    Options("Go Left,Go Right,Approach Cliff");
+    switch(Response()) {
+      case 1:
+        CallOption("leftPath")
+        break;
+      default:
+        NotAnOption();
+    }
+    
+  }
+
+  static void LeftPath() {
+    System.out.println("You take the path to your left.");
+  }
+
+
   static void NotAnOption() {
     System.out.println("That was not an option. Quitting game.");
     System.exit(0);
@@ -39,56 +79,16 @@ public class Game {
     System.out.println("What is your name?");
     String userName = in.nextLine();
     System.out.println(userName + ", are you ready to begin?");
-    options("Yes,No");
+    Options("Yes,No");
     int choice = Integer.parseInt(in.nextLine());
 
     //first choice
     if (choice == 1) {
       System.out.println("Then let us begin.");
       //begining the game
-      System.out.println("You find yourself on a stone platform in the middle of a clearing. Behind you there is a cliff. Ahead of you there are two paths.");
-      options("Go Right,Go Left,Approach Cliff");
-      choice = Integer.parseInt(in.nextLine());
-      switch (choice) {
-        case 1:
-          System.out.println("You chose to go right");
-          System.out.println("After walking down the path for a while, you start to see a stone wall ahead.\nThe wall is too high to see above. On the other side you can see a few pillars of smoke.");
-          options("Scale Wall,Call up to wall,walk around the wall");
-          choice = Integer.parseInt(in.nextLine());
-          switch (choice) {
-            case 1:
-              System.out.println("You attempt to scale the wall.\nAs you begin to climb, You hear a voice shout down at you.");
-              CharacterSpeak("Voice", "OI! What do you think you're doing?");
-              options("Reply: Climbing?,Reply: Slaying Dragons,Introduce Yourself,Stay Silent,Jump Down");
-              choice = Integer.parseInt(in.nextLine());
-              //Unfinished
-              break;
-            case 2:
-              System.out.println("You shout up to the wall. What do you shout?");
-              options("Hello?,Anyone Up There?,Who Dares Build A Wall In My Path?")
-              choice = Integer.parseInt(in.nextLine());
-              switch (choice)
-              //Unfinished
-              break;
-            case 3:
-              System.out.println("Walk left or right?");
-              options("Left,Right");
-              //Unfinished
-              break;
-            default:
-              NotAnOption();
-          }
-          break;
-        case 2:
-          System.out.println("You chose to go left");
-          break;
-        case 3:
-          System.out.println("You chose to approach the cliff");
-          break;
-        default:
-          NotAnOption();
+      CallOption("startPlatform");
       }
-    } else {
+    else {
       System.out.println("Then what the hell are you doing here " + userName + "?");
       System.exit(0);
     }
